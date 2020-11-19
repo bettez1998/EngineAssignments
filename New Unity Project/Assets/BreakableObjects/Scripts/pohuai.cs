00e5857ba98f36cf2696ff1a510679696c558f1c
bettez1998 <zhujunjie0812@gmail.com>
2020-11-19 21:01:05 +0800

    project

---
+++ New Unity Project/Assets/BreakableObjects/Scripts/pohuai.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pohuai : MonoBehaviour
+{
+    public GameObject pohuaiVersion;
+    
+    void OnMouseDown()
+    {
+        Instantiate(pohuaiVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        print("click");
+    }
+
+}
